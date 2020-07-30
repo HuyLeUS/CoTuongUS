8d8f90f0db5ff8fd5de73839a2d971ba1afe747c
meokg456 <meokg456@gmail.com>
2020-07-30 14:21:58 +0700

    Canh giữa bàn cờ

--- app/src/main/java/com/example/ctngus/DrawBoard.java
+++ app/src/main/java/com/example/ctngus/DrawBoard.java
@@ -1,68 +1,64 @@
 package com.example.ctngus;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 public class DrawBoard extends View {
 
-    private Paint paint=new Paint();
-    public DrawBoard(Context context) {
-        super(context);
-        init();
-    }
-    private  void init()
-    {
-        paint.setColor(Color.GRAY);
-        paint.setStrokeWidth(6);
-    }
+     private Paint paint = new Paint();
+     private int boardHeight;
+     private int boardWidth;
+     private int leftMargin;
+     private int topMargin;
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawLine(100,100,100,1000,paint);
-        canvas.drawLine(200,100,200 ,1000,paint);
-        canvas.drawLine(100,100,900,100,paint);
-        canvas.drawLine(100,200,900,200,paint);
-        canvas.drawLine(300,100,300,1000,paint);
-        canvas.drawLine(400,100 ,400 ,1000,paint);
-        canvas.drawLine(100,300,900,300,paint);
-        canvas.drawLine(100,400,900,400,paint);
-        canvas.drawLine(500,100,500,1000,paint);
-        canvas.drawLine(600,100 ,600 ,1000,paint);
-        canvas.drawLine(100,500,900,500,paint);
-        canvas.drawLine(100,600,900,600,paint);
-        canvas.drawLine(700,100,700,1000,paint);
-        canvas.drawLine(800,100 ,800 ,1000,paint);
-        canvas.drawLine(100,700,900,700,paint);
-        canvas.drawLine(100,800,900,800,paint);
-        canvas.drawLine(100,900,900,900,paint);
-        canvas.drawLine(900,100,900,1000,paint);
-        canvas.drawLine(100,1000,900,1000,paint);
+     public DrawBoard(Context context) {
+          super(context);
+          init();
+     }
 
-        canvas.drawLine(100,500,200,600,paint);
-        canvas.drawLine(200,500,300,600,paint);
-        canvas.drawLine(300,500,400,600,paint);
-        canvas.drawLine(400,500,500,600,paint);
-        canvas.drawLine(500,500,600,600,paint);
-        canvas.drawLine(600,500,700,600,paint);
-        canvas.drawLine(700,500,800,600,paint);
-        canvas.drawLine(800,500,900,600,paint);
+     private void init() {
+          DisplayMetrics displayMetrics = new DisplayMetrics();
+          ((Activity) getContext()).getWindowManager()
+                  .getDefaultDisplay()
+                  .getMetrics(displayMetrics);
+          boardWidth = (int) (displayMetrics.widthPixels * 0.88);
+          boardHeight = (int) (boardWidth / 8 * 9);
+          leftMargin = (displayMetrics.widthPixels - boardWidth) / 2;
+          topMargin = (displayMetrics.heightPixels - boardHeight) / 2;
+          paint.setColor(Color.GRAY);
+          paint.setStrokeWidth(6);
+     }
 
-        canvas.drawLine(100,600,200,500,paint);
-        canvas.drawLine(200,600,300,500,paint);
-        canvas.drawLine(300,600,400,500,paint);
-        canvas.drawLine(400,600,500,500,paint);
-        canvas.drawLine(500,600,600,500,paint);
-        canvas.drawLine(600,600,700,500,paint);
-        canvas.drawLine(700,600,800,500,paint);
-        canvas.drawLine(800,600,900,500,paint);
+     @Override
+     protected void onDraw(Canvas canvas) {
+         super.onDraw(canvas);
+         int side = boardWidth / 8;
+         canvas.drawLine(leftMargin, topMargin, leftMargin + boardWidth, topMargin, paint);
+         canvas.drawLine(leftMargin + boardWidth, topMargin, leftMargin + boardWidth, topMargin + boardHeight, paint);
+         canvas.drawLine(leftMargin + boardWidth, topMargin + boardHeight, leftMargin, topMargin + boardHeight, paint);
+         canvas.drawLine(leftMargin, topMargin + boardHeight, leftMargin, topMargin, paint);
+         // Vẽ các đường ngang
+         for (int i = 1; i < 9; i++) {
+             canvas.drawLine(leftMargin, topMargin + side * i, leftMargin + boardWidth, topMargin + side * i, paint);
+         }
+         // Vẽ các đường dọc
+         for (int i = 1; i < 8; i++)
+         {
+             canvas.drawLine(leftMargin + i*side, topMargin, leftMargin + i*side , topMargin+ 4 * side, paint);
+             canvas.drawLine(leftMargin + i*side, topMargin + 5 * side, leftMargin + i*side , topMargin + 9 * side, paint);
+         }
+         //Vẽ các đường chéo cấm cung
+         canvas.drawLine(leftMargin + 3 * side, topMargin, leftMargin + 5 * side, topMargin + 2 * side, paint);
+         canvas.drawLine(leftMargin + 5 * side, topMargin, leftMargin + 3 * side, topMargin + 2 * side, paint);
 
-        canvas.drawLine(400,100,600,300,paint);
-        canvas.drawLine(600,100,400,300,paint);
-        canvas.drawLine(400,800,600,1000,paint);
-        canvas.drawLine(600,800,400,1000,paint);
-    }
+         canvas.drawLine(leftMargin + 3 * side, topMargin + 7 * side, leftMargin + 5 * side, topMargin + 9 * side, paint);
+         canvas.drawLine(leftMargin + 5 * side, topMargin + 7 * side, leftMargin + 3 * side, topMargin + 9 * side, paint);
+
+
+     }
 }
